2db5440560f0f973b702a1e2c6c4607c0ff8e3cc
Sam Harlan <sams.email24@gmail.com>
2019-05-23 11:34:07 -0700

    update to port number

--- app-3.js
+++ app-3.js
@@ -6,6 +6,6 @@ app.use('/', myRouter);
 //Serves static content from directory public
 app.use('/', express.static('public'));
 
-app.listen(3000, function ServerListener() {
+app.listen(3010, function ServerListener() {
     console.log('Sample App with router. This app serves static content');
 });
