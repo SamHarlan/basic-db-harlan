ce0be2f380aa5fcfe04e7e5e21a372a4250fad1f
Sam Harlan <sams.email24@gmail.com>
2019-05-24 18:34:17 -0700

    all

--- employees.router.js
+++ employees.router.js
@@ -140,7 +140,7 @@ router.get('/salaries', function (request, response) {
           '<tr> \n' +          
           '	<td>' + record["first_name"] + '</td> \n' +
           '	<td>' + record["last_name"] + '</td> \n' +
-          '	<td>' + record["dept_name"] + '</td> \n' +
+          '	<td>' + record["dept_emp"] + '</td> \n' +
           '</tr> \n'
         );
       }
@@ -199,11 +199,54 @@ router.get('/salaries', function (request, response) {
       });
 
       router.get('/all', function (request, response) {
-        db.get().query('SELECT * FROM employees LIMIT 30',
+        db.get().query("SELECT first_name,last_name,emp_no,departments,dept_emp where employees.emp_no = dept_emp.emp_no and dept_emp.dept_no = departments.dept_no,salary from employees join salaries on employees.emp_no=salaries.emp_no FROM employees,B.first_name as boss_first_name, B.last_name as boss_last_name from employees E, dept_emp DE, departments D, dept_manager DM, employees B where E.emp_no = DE.emp_no and DE.dept_no=D.dept_no and DE.to_date='9999-01-01' and DM.to_date='9999-01-01' and D.dept_no=DM.dept_no and B.emp_no=DM.emp_no  LIMIT 30",
           function SelectQueryHandler(err, result, fields){
             if (err)
             return done(err);
-            response.send(result);
+            //response.send(result);
+            response.write(
+              '<!DOCTYPE html> \n' +
+              '<html lang="en"> \n' +
+              '        <head> \n' +
+              '               <meta charset="utf-8"> \n' +
+              '               <meta http-equiv="X-UA-Compatible" content="IE=edge"> \n' +
+              '               <meta name="viewport" content="width=device-width, initial-scale=1"> \n' +
+              '               <meta name="description" content="30 Employees"> \n' +
+              '               <meta name="author" content="Sam Harlan"> \n' +
+              '               <script type="text/javascript" src="buses.js"></script> \n' +
+              '               <title>Client Side Example</title> \n' +
+              '               <!-- Bootstrap core CSS --> \n' +
+              '               <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> \n' +
+              '       </head> \n' +
+              '<table class="table table-bordered table-hover"> \n' +
+              '	<thead class = "thead-dark"> \n' +
+              '		<tr> \n' +
+              '			<th scope="col">Employee #</th> \n' +
+              '			<th scope="col">Employee First Name</th> \n' +
+              '			<th scope="col">Employee Last Name</th> \n' +
+              '     <th scope="col">dept_name</th> \n' + 
+              '     <th scope="col">Salary</th> \n' + 
+              '     <th scope="col">boss first name</th> \n' + 
+              '     <th scope="col">boss last name</th> \n' + 
+              '		</tr> \n' +
+              '	</thead> \n' +
+              '	<tbody> \n'
+            );
+            for (record of result){
+              response.write(
+                '<tr> \n' +  
+                '	<td>' + record["emp_no"] + '</td> \n' +        
+                '	<td>' + record["emp_first_name"] + '</td> \n' +
+                '	<td>' + record["emp_last_name"] + '</td> \n' +
+                '	<td>' + record["dept_name"] + '</td> \n' +
+                '	<td>' + record["salary"] + '</td> \n' +
+                '	<td>' + record["boss_first_name"] + '</td> \n' +
+                '	<td>' + record["boss_last_name"] + '</td> \n' +
+                '</tr> \n'
+              );
+            }
+            response.write("</tbody></table></body></html>");
+            response.end();
           });
         });
 
