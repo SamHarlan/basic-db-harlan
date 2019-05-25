edfe6d33873df08407bd809f661cc9545500ba77
Sam Harlan <sams.email24@gmail.com>
2019-05-25 10:43:26 -0700

    edit
    edit
    
    
    new
    
    
    working!!
    
    
    edit
    
    
    update
    
    
    check
    
    
    change
    
    
    edit
    
    
    no idea
    
    
    same
    
    
    same
    
    
    update
    
    
    maybe?
    
    
    edit
    
    
    check boss
    
    
    again
    
    
    bossy!
    
    
    all
    
    
    all
    
    
    all
    
    
    all
    
    
    all
    
    
    all
    
    
    hellpo
    
    
    testing
    
    
    all add emp-no
    
    
    NO IDEA
    
    
    add no to all
    
    
    back
    
    
    new
    
    
    maybe final?
    
    
    align title center
    
    
    do you really need style?
    
    
    no style needed!
    
    
    center heading

--- employees.router.js
+++ employees.router.js
@@ -1,82 +1,59 @@
 var express = require('express');	// imports the express library
 var router = express.Router();		// Router object for routes
-var db = require('./db.js');		// Get access to the Database
+var db = require('./db.js');
 
 
 // Setting home route response
 router.get('/', function handleRootGet(request, response) {
- // response.sendfile('public/index.html');
-  //response.send('Home Page');
-  '<!DOCTYPE html> \n' +
-      '<html lang="en" dir="ltr"> \n' +
-      '  <head> \n' +
-      '    <meta charset="utf-8"> \n' +
-      '    <title>MySQL Assignment</title> \n' +
-      '    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> \n' +
-      '  </head> \n' +
-      '  <body> \n' +
-      '      <div class="container"> \n' + '<br>' + '<br>' +
-      '       <h1>Welcome!</h1> \n' +
-      '       <h5>Choose a button that you want to be directed..</h5> \n' + '<br>' +
-      '       <a href="/employees" class="btn btn-secondary btn-lg" role="button" aria-disabled="true">View Employees</a> \n' +
-      '       <a href="/salaries" class="btn btn-secondary btn-lg" role="button" aria-disabled="true">View Salaries</a> \n' +
-      '       <a href="/departments" class="btn btn-secondary btn-lg" role="button" aria-disabled="true">View Departments</a> \n' +
-      '       <a href="/bosses" class="btn btn-secondary btn-lg" role="button" aria-disabled="true">View Bosses</a> \n' +
-      '       <a href="/all" class="btn btn-secondary btn-lg" role="button" aria-disabled="true">View All</a> \n' +
-      '      </div> \n' +
-      '  </body> \n' +
-      '</html> \n'
-
-      response.send('Home Page');
+  db.connect(function ConnectToDatabase(err){
+    if (err){
+      console.log("Unable to Connect to MySQL");
+    }
+  });
+  response.sendfile('public/index.html');
   // Create a static page with links to all 5 requests, or serve that page here
 });
 
 router.get('/employees', function (request, response) {
-  db.connect(function ConnectToDatabase(err){
-    if (err){
-      console.log("Unable to Connect to MySQL");
-      process.exit(1); //Possibly need to send error page to client
-    }
-    db.get().query("SELECT first_name,last_name,emp_no FROM employees LIMIT 30", function QueryHandler(err, result, fields){
-      if (err)
-      throw err;
-      //response.send(JSON.stringify(result));
+  db.get().query("SELECT first_name,last_name,emp_no FROM employees LIMIT 30", function QueryHandler(err, result, fields){
+    if (err)
+    throw err;
+    
+    response.write(
+      '<!DOCTYPE html> \n' +
+      '<html lang="en"> \n' +
+      '        <head> \n' +
+      '               <meta charset="utf-8"> \n' +
+      '               <meta http-equiv="X-UA-Compatible" content="IE=edge"> \n' +
+      '               <meta name="viewport" content="width=device-width, initial-scale=1"> \n' +
+      '               <meta name="description" content="30 Employees"> \n' +
+      '               <meta name="author" content="Sam Harlan"> \n' +
+      '               <script type="text/javascript" src="employees.router.js"></script> \n' +
+      '               <title>Employees</title> \n' +
+      '               <!-- Bootstrap core CSS --> \n' +
+      '               <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> \n' +
+      '       </head> \n' +
+      '<table class="table table-bordered table-hover"> \n' +
+      '	<thead class = "thead-dark"> \n' +
+      '		<tr> \n' +
+      '			<th scope="col">Employee #</th> \n' +
+      '			<th scope="col">First Name</th> \n' +
+      '			<th scope="col">Last Name</th> \n' +
+      '		</tr> \n' +
+      '	</thead> \n' +
+      '	<tbody> \n'
+    );
+    for (record of result){
       response.write(
-        '<!DOCTYPE html> \n' +
-        '<html lang="en"> \n' +
-        '        <head> \n' +
-        '               <meta charset="utf-8"> \n' +
-        '               <meta http-equiv="X-UA-Compatible" content="IE=edge"> \n' +
-        '               <meta name="viewport" content="width=device-width, initial-scale=1"> \n' +
-        '               <meta name="description" content="30 Employees"> \n' +
-        '               <meta name="author" content="Lhakpa Sherpa"> \n' +
-        '               <script type="text/javascript" src="buses.js"></script> \n' +
-        '               <title>Client Side Example</title> \n' +
-        '               <!-- Bootstrap core CSS --> \n' +
-        '               <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> \n' +
-        '       </head> \n' +
-        '<table class="table table-bordered table-hover"> \n' +
-        '	<thead class = "thead-dark"> \n' +
-        '		<tr> \n' +
-        '			<th scope="col">Employee #</th> \n' +
-        '			<th scope="col">First Name</th> \n' +
-        '			<th scope="col">Last Name</th> \n' +
-        '		</tr> \n' +
-        '	</thead> \n' +
-        '	<tbody> \n'
+        '<tr> \n' +
+        '	<td>' + record["emp_no"] + '</td> \n' +
+        '	<td>' + record["first_name"] + '</td> \n' +
+        '	<td>' + record["last_name"] + '</td> \n' +
+        '</tr> \n'
       );
-      for (record of result){
-        response.write(
-          '<tr> \n' +
-          '	<td>' + record["emp_no"] + '</td> \n' +
-          '	<td>' + record["first_name"] + '</td> \n' +
-          '	<td>' + record["last_name"] + '</td> \n' +
-          '</tr> \n'
-        );
-      }
-      response.write("</tbody></table></body></html>");
-      response.end();
-    });
+    }
+    response.write("</tbody></table></body></html>");
+    response.end();
   });
 });
 
@@ -86,7 +63,42 @@ router.get('/salaries', function (request, response) {
     function SelectQueryHandler(err, result, fields){
       if (err)
       return done(err);
-      response.send(result);
+
+      response.write(
+        '<!DOCTYPE html> \n' +
+        '<html lang="en"> \n' +
+        '        <head> \n' +
+        '               <meta charset="utf-8"> \n' +
+        '               <meta http-equiv="X-UA-Compatible" content="IE=edge"> \n' +
+        '               <meta name="viewport" content="width=device-width, initial-scale=1"> \n' +
+        '               <meta name="description" content="30 Employees"> \n' +
+        '               <meta name="author" content="Sam Harlan"> \n' +
+        '               <script type="text/javascript" src="employees.router.js"></script> \n' +
+        '               <title>Salaries</title> \n' +
+        '               <!-- Bootstrap core CSS --> \n' +
+        '               <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> \n' +
+        '       </head> \n' +
+        '<table class="table table-bordered table-hover"> \n' +
+        '	<thead class = "thead-dark"> \n' +
+        '		<tr> \n' +
+        '			<th scope="col">First Name</th> \n' +
+        '			<th scope="col">Last Name</th> \n' +
+        '     <th scope="col">Salary</th> \n' + 
+        '		</tr> \n' +
+        '	</thead> \n' +
+        '	<tbody> \n'
+      );
+      for (record of result){
+        response.write(
+          '<tr> \n' +          
+          '	<td>' + record["first_name"] + '</td> \n' +
+          '	<td>' + record["last_name"] + '</td> \n' +
+          '	<td>' + record["salary"] + '</td> \n' +
+          '</tr> \n'
+        );
+      }
+      response.write("</tbody></table></body></html>");
+      response.end();
     });
   });
 
@@ -96,29 +108,148 @@ router.get('/salaries', function (request, response) {
       function SelectQueryHandler(err, result, fields){
         if (err)
         return done(err);
-        response.send(result);
+       
+       response.write(
+        '<!DOCTYPE html> \n' +
+        '<html lang="en"> \n' +
+        '        <head> \n' +
+        '               <meta charset="utf-8"> \n' +
+        '               <meta http-equiv="X-UA-Compatible" content="IE=edge"> \n' +
+        '               <meta name="viewport" content="width=device-width, initial-scale=1"> \n' +
+        '               <meta name="description" content="30 Employees"> \n' +
+        '               <meta name="author" content="Sam Harlan"> \n' +
+        '               <script type="text/javascript" src="employees.router.js"></script> \n' +
+        '               <title>Departments</title> \n' +
+        '               <!-- Bootstrap core CSS --> \n' +
+        '               <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> \n' +
+        '       </head> \n' +
+        '<table class="table table-bordered table-hover"> \n' +
+        '	<thead class = "thead-dark"> \n' +
+        '		<tr> \n' +
+        '			<th scope="col">First Name</th> \n' +
+        '			<th scope="col">Last Name</th> \n' +
+        '     <th scope="col">dept_name</th> \n' + 
+        '		</tr> \n' +
+        '	</thead> \n' +
+        '	<tbody> \n'
+      );
+      for (record of result){
+        response.write(
+          '<tr> \n' +          
+          '	<td>' + record["first_name"] + '</td> \n' +
+          '	<td>' + record["last_name"] + '</td> \n' +
+          '	<td>' + record["dept_name"] + '</td> \n' +
+          '</tr> \n'
+        );
+      }
+      response.write("</tbody></table></body></html>");
+      response.end();
       });
     });
 
     router.get('/bosses', function (request, response) {
       db.get().query(
-        'SELECT emp_no, first_name FROM employees LIMIT 30',
+        'select E.first_name as emp_first_name, E.last_name as emp_last_name, dept_name, B.first_name as boss_first_name, B.last_name as boss_last_name from employees E, dept_emp DE, departments D, dept_manager DM, employees B where E.emp_no = DE.emp_no and DE.dept_no=D.dept_no and DE.to_date="9999-01-01" and DM.to_date="9999-01-01" and D.dept_no=DM.dept_no and B.emp_no=DM.emp_no LIMIT 30',
         function SelectQueryHandler(err, result, fields){
           if (err)
           return done(err);
-          response.send(result);
+          
+          response.write(
+            '<!DOCTYPE html> \n' +
+            '<html lang="en"> \n' +
+            '        <head> \n' +
+            '               <meta charset="utf-8"> \n' +
+            '               <meta http-equiv="X-UA-Compatible" content="IE=edge"> \n' +
+            '               <meta name="viewport" content="width=device-width, initial-scale=1"> \n' +
+            '               <meta name="description" content="30 Employees"> \n' +
+            '               <meta name="author" content="Sam Harlan"> \n' +
+            '               <script type="text/javascript" src="employees.router.js"></script> \n' +
+            '               <title>Bosses</title> \n' +
+            '               <!-- Bootstrap core CSS --> \n' +
+            '               <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> \n' +
+            '       </head> \n' +
+            '<table class="table table-bordered table-hover"> \n' +
+            '	<thead class = "thead-dark"> \n' +
+            '		<tr> \n' +
+            '			<th scope="col">Employee First Name</th> \n' +
+            '			<th scope="col">Employee Last Name</th> \n' +
+            '     <th scope="col">dept_name</th> \n' + 
+            '     <th scope="col">boss first name</th> \n' + 
+            '     <th scope="col">boss last name</th> \n' + 
+            '		</tr> \n' +
+            '	</thead> \n' +
+            '	<tbody> \n'
+          );
+          for (record of result){
+            response.write(
+              '<tr> \n' +          
+              '	<td>' + record["emp_first_name"] + '</td> \n' +
+              '	<td>' + record["emp_last_name"] + '</td> \n' +
+              '	<td>' + record["dept_name"] + '</td> \n' +
+              '	<td>' + record["boss_first_name"] + '</td> \n' +
+              '	<td>' + record["boss_last_name"] + '</td> \n' +
+              '</tr> \n'
+            );
+          }
+          response.write("</tbody></table></body></html>");
+          response.end();
         });
       });
 
       router.get('/all', function (request, response) {
+        
         db.get().query(
-          'SELECT emp_no, first_name FROM employees LIMIT 30',
-          function SelectQueryHandler(err, result, fields){
+          'select E.first_name as emp_first_name, E.last_name as emp_last_name, dept_name, B.first_name as boss_first_name, B.last_name as boss_last_name from employees E, dept_emp DE, departments D, dept_manager DM, employees B where E.emp_no = DE.emp_no and DE.dept_no=D.dept_no and DE.to_date="9999-01-01" and DM.to_date="9999-01-01" and D.dept_no=DM.dept_no and B.emp_no=DM.emp_no LIMIT 30',
+          
+        function SelectQueryHandler(err, result, fields){
             if (err)
             return done(err);
-            response.send(result);
+            
+            response.write(
+              '<!DOCTYPE html> \n' +
+              '<html lang="en"> \n' +
+              '        <head> \n' +
+              '               <meta charset="utf-8"> \n' +
+              '               <meta http-equiv="X-UA-Compatible" content="IE=edge"> \n' +
+              '               <meta name="viewport" content="width=device-width, initial-scale=1"> \n' +
+              '               <meta name="description" content="30 Employees"> \n' +
+              '               <meta name="author" content="Sam Harlan"> \n' +
+              '               <script type="text/javascript" src="employees.router.js"></script> \n' +
+              '               <title>All</title> \n' +
+              '               <!-- Bootstrap core CSS --> \n' +
+              '               <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> \n' +
+              '       </head> \n' +
+              '<table class="table table-bordered table-hover"> \n' +
+              '	<thead class = "thead-dark"> \n' +
+              '		<tr> \n' +
+              '			<th scope="col">Employee #</th> \n' +
+              '			<th scope="col">Employee First Name</th> \n' +
+              '			<th scope="col">Employee Last Name</th> \n' +
+              '     <th scope="col">dept_name</th> \n' + 
+              '     <th scope="col">Salary</th> \n' + 
+              '     <th scope="col">boss first name</th> \n' + 
+              '     <th scope="col">boss last name</th> \n' + 
+              '		</tr> \n' +
+              '	</thead> \n' +
+              '	<tbody> \n'
+            );
+            for (record of result){
+              response.write(
+                '<tr> \n' +  
+                '	<td>' + record["emp_no"] + '</td> \n' +        
+                '	<td>' + record["first_name"] + '</td> \n' +
+                '	<td>' + record["last_name"] + '</td> \n' +
+                '	<td>' + record["dept_name"] + '</td> \n' +
+                '	<td>' + record["salary"] + '</td> \n' +
+                '	<td>' + record["boss_first_name"] + '</td> \n' +
+                '	<td>' + record["boss_last_name"] + '</td> \n' +
+                '</tr> \n'
+              );
+            }
+            response.write("</tbody></table></body></html>");
+            response.end();
           });
         });
 
-        // Exporting the router "object"
+        
         module.exports = router;
